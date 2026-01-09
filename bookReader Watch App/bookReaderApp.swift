166bd89b45cead80298e0fa6e67abe0fb531450d
Misha Dovhiy <44978117+mishadovhiy@users.noreply.github.com>
2026-01-09 18:48:15 +0200

    first commit

---
+++ bookReader Watch App/bookReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookReaderApp.swift
+//  bookReader Watch App
+//
+//  Created by Mykhailo Dovhyi on 09.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct bookReader_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
